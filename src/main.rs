104018f5de411ba5569f08c465749c4e5035ed9c
PraveenRoyal <praveenpp2929@gmail.com>
2024-06-27 15:54:19 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ fn main() {
     println!("Give the Name of the Directory which contains all the file of the assignment : ");
     std::io::stdin().read_line(&mut store).expect("Invalid File Name");
     let mut path = PathBuf::new();
-    path.push(store.as_str());
+    path.push(store.trim());
     directory_parser(&mut path);
 }
